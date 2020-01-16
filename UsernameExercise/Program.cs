9a571e15109beeb4ae84cd3c25865967b380e44b
rhi-teetzel <rhiannateetzel@gmail.com>
2020-01-15 20:38:01 -0500

    First commit

---
+++ UsernameExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UsernameExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string newPassword;
+            string passwordVerification;
+
+            Console.WriteLine("Please enter your new password.");
+            newPassword = Console.ReadLine();
+            Console.WriteLine("Please re-enter your password for verification.");
+            passwordVerification = Console.ReadLine();
+
+
+            if (newPassword == passwordVerification)
+            {
+                Console.WriteLine("Your new password has been accepted!");
+            }
+            else
+            {
+                Console.WriteLine("Your verification did not match.");
+            }
+            Console.ReadLine();
+        }   
+
+    }
+}
